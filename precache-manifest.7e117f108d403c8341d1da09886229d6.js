b9e7c4647fe81a9d6f0390f3c8cc6639484d858d
sgzf <312376873@qq.com>
2019-06-01 15:07:14 +0800

    vue-blog

---
+++ precache-manifest.7e117f108d403c8341d1da09886229d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3470bedbb168c7955225",
+    "url": "/public/css/app.70d0eec8.css"
+  },
+  {
+    "revision": "3470bedbb168c7955225",
+    "url": "/public/js/app.9ee89ff7.js"
+  },
+  {
+    "revision": "4834a7aed891a0c302ca",
+    "url": "/public/js/chunk-vendors.6cf986f9.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/public/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a98f7fe9802745716e5c8ca8bc049cd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b8fc210240dab77165e53ba0ce6c98a5",
+    "url": "/static/posts.json"
+  }
+];
